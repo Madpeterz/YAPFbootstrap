028c47d318da9bdb83cf1fc90432855745ce97a5
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2022-02-10 05:03:45 +0000

    remove old code and point to simpleconfig

--- src/Bootstrap/ConfigBox/ConfigBox.php
+++ src/Bootstrap/ConfigBox/ConfigBox.php
@@ -2,16 +2,10 @@
 
 namespace YAPF\Bootstrap\ConfigBox;
 
-use YAPF\Cache\Cache;
-use YAPF\Cache\Drivers\Disk;
-use YAPF\Cache\Drivers\Redis;
-use YAPF\MySQLi\MysqliEnabled;
+use YAPF\Config\SimpleConfig;
 
-class BootstrapConfigBox extends ErrorLogging
+class BootstrapConfigBox extends SimpleConfig
 {
-    // SQL connection
-    protected MysqliEnabled $sql;
-
     // url switchs
     protected string $page = "";
     protected string $module = "";
@@ -27,42 +21,12 @@ class BootstrapConfigBox extends ErrorLogging
     protected string $rootfolder = "../";
     protected string $deepFolder = "../../";
 
-    // Cache
-    protected ?Cache $Cache;
-    protected bool $cache_enabled = false;
-
-    // Cache / Disk
-    protected bool $use_disk_cache = false;
-    protected string $disk_cache_folder = "cache";
-
-    // Cache / Redis
-    protected bool $use_redis_cache = false;
-
-    // Cache / Redis / Unix socket
-    protected bool $redisUnix = false;
-    protected string $redis_socket = "/var/run/redis/redis.sock";
-
-    // Cache / Redis / TCP
-    protected string $redis_host = "redis";
-    protected int $redis_port = 6379;
-    protected int $redis_timeout = 3;
-
-    // docker flag
-    protected bool $dockerConfigLocked = false;
-
     // config Flags
     protected array $flags = [];
 
     public function __construct()
     {
-        if (class_exists("App\\Db", false) == false) {
-            $offline = [
-                "status" => 0,
-                "message" => "- Service offline -<br/> DB config missing",
-            ];
-            die(json_encode($offline));
-        }
-        $this->sql = new MysqliEnabled();
+        parent::__construct();
         $this->setFlag("SITE_NAME", "bootstrap enabled");
         $this->setFlag("SITE_URL", "http://localhost/");
         $this->setFlag("url_base", $this->getFlag("SITE_URL"));
@@ -77,8 +41,17 @@ class BootstrapConfigBox extends ErrorLogging
                 $this->configCacheRedisTCP(getenv("SITE_CACHE_REDIS_HOST"));
             }
             $this->dockerConfigLocked = true; // disable all config functions
+            $this->setupCache();
+            $this->setupCacheTables();
             $this->startCache();
         }
+    }
+
+    /*
+        Tables to enable with cache
+    */
+    protected function setupCacheTables(): void
+    {
     }
 
     /*
@@ -120,95 +93,6 @@ class BootstrapConfigBox extends ErrorLogging
         }
         return $this->flags[$flagName];
     }
-
-    /*
-        SQL functions
-    */
-    public function &getSQL(): MysqliEnabled
-    {
-        return $this->sql;
-    }
-
-    /*
-        Cache functions
-    */
-    public function &getCacheDriver(): ?Cache
-    {
-        return $this->Cache;
-    }
-
-    public function configCacheDisabled(): void
-    {
-        if ($this->dockerConfigLocked == true) {
-            return;
-        }
-        $this->use_redis_cache = false;
-        $this->use_disk_cache = false;
-    }
-    public function configCacheRedisUnixSocket(string $socket = "/var/run/redis/redis.sock"): void
-    {
-        if ($this->dockerConfigLocked == true) {
-            return;
-        }
-        $this->use_redis_cache = true;
-        $this->redisUnix = true;
-        $this->redis_socket = $socket;
-    }
-    public function configCacheRedisTCP(string $host = "redis", int $port = 6379, int $timeout = 3): void
-    {
-        if ($this->dockerConfigLocked == true) {
-            return;
-        }
-        $this->use_redis_cache = true;
-        $this->redisUnix = false;
-        $this->redis_host = $host;
-        $this->redis_port = $port;
-        $this->redis_timeout = $timeout;
-    }
-    public function configCacheDisk(string $folder = "cache"): void
-    {
-        if ($this->dockerConfigLocked == true) {
-            return;
-        }
-        $this->use_disk_cache = true;
-        $this->disk_cache_folder = $folder;
-    }
-
-    public function startCache(): void
-    {
-        $this->Cache = null;
-        if ($this->use_redis_cache == true) {
-            $this->startRedisCache();
-        }
-        if ($this->use_disk_cache == true) {
-            $this->startDiskCache();
-        }
-        $this->leakCache();
-        return;
-    }
-
-    protected function startRedisCache(): void
-    {
-        $this->Cache = new Redis();
-        if ($this->redisUnix == true) {
-            $this->Cache->connectUnix($this->redis_socket);
-            return;
-        }
-        $this->Cache->setTimeout($this->redis_timeout);
-        $this->Cache->connectTCP($this->redis_host, $this->redis_port);
-    }
-
-    protected function leakCache(): void
-    {
-        global $cache; // old mode support [to be phased out]
-        $cache = $this->Cache;
-    }
-
-    protected function startDiskCache(): void
-    {
-        $this->Cache = new Disk($this->rootfolder . "/" . $this->disk_cache_folder);
-    }
-
 
     /*
         URL loading
