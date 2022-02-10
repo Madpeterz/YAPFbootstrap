9f5c5e1e23be5a84e04e0fd860486a2fa314d00a
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2022-02-10 05:29:10 +0000

    Update Switchboard.php

--- src/Bootstrap/Switchboard/Switchboard.php
+++ src/Bootstrap/Switchboard/Switchboard.php
@@ -13,9 +13,13 @@ abstract class Switchboard extends ErrorLogging
     protected string $targetEndpoint = "";
     protected ?View $loadedObject;
 
-    public function __construct(&BootstrapConfigBox $config)
+    protected string $loadingModule = "";
+    protected string $loadingArea = "";
+
+    public function __construct()
     {
-        $this->config = $config;
+        global $system;
+        $this->config = $system;
         $this->loadPage();
     }
 
@@ -35,13 +39,13 @@ abstract class Switchboard extends ErrorLogging
     protected function findMasterClass(int $step = 0): ?string
     {
         $args = [
-            $this->config->getModule(),
-            $this->config->getArea(),
+            $this->loadingModule,
+            $this->loadingArea,
             $this->config->getPage(),
             $this->config->getOption(),
         ];
         if ($step == 4) {
-            $bits = ["App","Endpoint",$this->targetEndpoint,$this->config->getModule(),"DefaultView"];
+            $bits = ["App","Endpoint",$this->targetEndpoint,$this->loadingModule,"DefaultView"];
             $use_class = "\\" . implode("\\", $bits);
             if (class_exists($use_class) == false) {
                 return null;
@@ -64,8 +68,11 @@ abstract class Switchboard extends ErrorLogging
 
     protected function loadPage(): void
     {
-        if ($this->notSet($this->config->getModule()) == true) {
-            $this->config->setModule("Home");
+        $this->loadingModule = $this->config->getModule();
+        $this->loadingArea = $this->config->getArea();
+
+        if ($this->notSet($this->loadingModule) == true) {
+            $this->loadingModule = "Home";
         }
 
         if ($this->accessChecks() == false) {
@@ -77,16 +84,16 @@ abstract class Switchboard extends ErrorLogging
                 ]);
             return;
         }
-        if (in_array($this->config->getArea(), ["","*"]) == true) {
-            $this->config->setArea("DefaultView")
+        if (in_array($this->loadingArea, ["","*"]) == true) {
+            $this->loadingArea = "DefaultView";
         }
         $use_class = $this->findMasterClass();
         if (class_exists($use_class) == false) {
             $this->addError("Unsupported request");
             print json_encode([
             "status" => "0",
-            "message" => "[" . $this->config->getModule() . " | "
-            . $this->config->getArea() . " | " . $this->config->getPage() . "] Unsupported",
+            "message" => "[" . $this->loadingModule . " | "
+            . $this->loadingArea . " | " . $this->config->getPage() . "] Unsupported",
             ]);
             http_response_code(501);
             return;
@@ -106,8 +113,8 @@ abstract class Switchboard extends ErrorLogging
 
     protected function fininalize($obj): void
     {
-        $this->loadedObject->getOutputObject()->setSwapTag("module", $this->config->getModule());
-        $this->loadedObject->getOutputObject()->setSwapTag("area", $this->config->getArea());
+        $this->loadedObject->getOutputObject()->setSwapTag("module", $this->loadingModule);
+        $this->loadedObject->getOutputObject()->setSwapTag("area", $this->loadingArea);
         $this->loadedObject->process();
     }
 }
