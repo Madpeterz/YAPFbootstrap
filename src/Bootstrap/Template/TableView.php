0a18ae5cf2c7f3285e8f30a29648461e8445c382
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2024-06-06 22:44:57 +0100

    force tableResponsive to true and add support to turn off

--- src/Bootstrap/Template/TableView.php
+++ src/Bootstrap/Template/TableView.php
@@ -4,6 +4,11 @@ namespace YAPF\Bootstrap\Template;
 
 class TableView extends BasicView
 {
+    protected bool $tableResponsive = true;
+    public function setTableResponsive(bool $status): void
+    {
+        $this->tableResponsive = $status;
+    }
     public function renderTable(
         array $table_head,
         array $table_body,
@@ -11,6 +16,9 @@ class TableView extends BasicView
         bool $show_head = true
     ): string {
         $output = '<table class="' . $classaddon . ' table table-striped">';
+        if ($this->tableResponsive == true) {
+            $output = '<div class="table-responsive">' . $output;
+        }
         if ($show_head == true) {
             $output .= '<thead><tr>';
             foreach ($table_head as $entry) {
@@ -31,6 +39,9 @@ class TableView extends BasicView
         }
         $output .= '</tbody>';
         $output .= '</table>';
+        if ($this->tableResponsive == true) {
+            $output .= "</div>";
+        }
         return $output;
     }
 }
